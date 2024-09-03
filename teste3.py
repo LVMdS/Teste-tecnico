eeba0675b4dac7432899204090a6fac8b834fbec
Leonardo Vinicius Martins de Souza <87584069+LVMdS@users.noreply.github.com>
2024-09-03 12:56:12 -0300

    Create teste3.py

---
+++ teste3.py
@@ -0,0 +1,31 @@
+INDICE = 12
+SOMA = 0
+K = 1
+
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+print(SOMA)
+
+
+'''
+Inicialização:
+
+INDICE é definido como 12.
+SOMA começa em 0.
+K começa em 1.
+Loop while:
+
+Enquanto K for menor que INDICE, o loop continua.
+K é incrementado em 1 a cada iteração.
+SOMA é atualizada somando o valor atual de K.
+Resultado Final:
+
+Ao final do loop, o valor de SOMA será o resultado final que será impresso.
+Resultado:
+Ao executar o código, o valor final da variável SOMA será 78.
+
+Isso ocorre porque o loop soma os números de 2 a 12 (ou seja, 2 + 3 + ... + 12 = 78).
+
+'''
